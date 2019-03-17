f7b22615d1d86e2cab215a3055a5886c746a8db2
Ryuu <yhwsr92@gmail.com>
2019-03-17 17:53:15 +0900

    unpacking, argument variable

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+#Read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("Script Name :", script)
+print("First Variable :", first)
+print("Second Variable :", second)
+print("Third Variable :", third)
