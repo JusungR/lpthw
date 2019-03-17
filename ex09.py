c3bb98da12fa885d826fd9e4c084ea24e2560e76
Ryuu <yhwsr92@gmail.com>
2019-03-17 16:15:41 +0900

    This is another test.

---
+++ ex09.py
@@ -0,0 +1,14 @@
+#처음 보는 낯선 내용이 있으니, 정확히 입력하도록 하세요.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApril\nMay\nJun\nJul\nAug"
+
+print("Here are days : ", days)
+print("Here are months : ", months)
+
+print("""
+There's something going on Here
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
